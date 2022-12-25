5f5028b65e1ee9cc6765997cb1ae5658328157a3
ahmetthakan <113829539+ahmetthakan@users.noreply.github.com>
2022-12-25 14:11:28 +0300

    Add files via upload

--- rtl/getir/Dallanma_Ongoru_Blogu.v
+++ rtl/getir/Dallanma_Ongoru_Blogu.v
@@ -30,12 +30,27 @@ module Dallanma_Ongoru_Blogu(
     input   [31:0]      buyruk_i, 
 
     // Atlama sonucunu belirten sinyaller
+    /* Bu kýsým program sayacý üreticisine girecek */
     output  [31:0]      atlanan_ps_o,       // Atlanilacak olan program sayaci
-    output              ongoru_gecerli_o   // Ongoru gecerli
+    output              ongoru_gecerli_o,   // Ongoru gecerli
 	
+	// Dallanmada hata olursa
+	input dallanma_hata_i,
+	/* Bu kýsým program sayacý üreticisine girecek */
+	output [31:0] ps_duzeltme,
+	output ps_duzeltme_aktif
 
     );
     
+    // Dallanmada hata olursa
+    reg [11:0]dallanma_gecmis_imm_r[3:0];
+    reg [31:0]dallanma_gecmis_ps_r[3:0];
+    reg [1:0] dg_idx = 0;
+    reg [1:0] dg_hata_idx = 0;
+    reg [31:0] ps_duzeltme_r = 0;
+    assign ps_duzeltme = ps_duzeltme_r;
+    reg ps_duzeltme_aktif_r = 0;
+    assign ps_duzeltme_aktif = ps_duzeltme_aktif_r;
     
     reg [22:0] etiket_r [127:0];
 	reg [1:0] durum_r [127:0];
@@ -56,7 +71,7 @@ module Dallanma_Ongoru_Blogu(
 	
 	// Etiket
 	wire [22:0] etiket;
-	assign etiket = ps_i[32:9]; 
+	assign etiket = ps_i[31:9]; 
 	
 	// Durumlar
     localparam GT = 2'b00;
@@ -68,7 +83,8 @@ module Dallanma_Ongoru_Blogu(
 	reg ongoru_gecerli_o_r = 0;
 	
 	assign atlanan_ps_o = atlanan_ps_o_r;
-	assign ongoru_gecerli_o = ongoru_gecerli_o_r;
+	// Ps duzeltilecekse ongoru gecersiz olmali
+	assign ongoru_gecerli_o = ps_duzeltme_aktif ? 1'b0 : ongoru_gecerli_o_r; 
 
 	// TEST ÝÇÝN
     reg [31:0]atlamaz_tahmin = 0;
@@ -89,6 +105,11 @@ module Dallanma_Ongoru_Blogu(
 			etiket_r[i] = 0;
 			durum_r[i] = 0;
 			hedef_adres_r[i] = 0;
+		end
+		
+		for(i=0 ; i<3 ; i=i+1) begin
+            dallanma_gecmis_imm_r[i] = 0;
+            dallanma_gecmis_ps_r[i] = 0;
 		end
 	
 	end
@@ -148,10 +169,14 @@ module Dallanma_Ongoru_Blogu(
 	
 	if(ongoru_aktif_i) begin
 		
+		
 		if(etiket == etiket_r[an_str_idx]) begin
 			ongoru_gecerli_o_r = 1'b1;
 			
 			if(durum_r[an_str_idx][1]) begin // ATLAR
+			
+			     
+			
 			    $display("girdi");
 				atlar_tahmin_ns = atlar_tahmin + 1'b1;
 				hedef_adres_buf = ps_i + {20'hFFFFF,Br_imm};
@@ -198,6 +223,36 @@ module Dallanma_Ongoru_Blogu(
         atlar_tahmin <= atlar_tahmin_ns; 
         atlamadi <= atlamadi_ns;
         atladi <= atladi_ns;
+        
+        if(guncelle_gecerli_i && guncelle_atladi_i) begin
+            dg_hata_idx <= dg_hata_idx +1'b1;  // 3->0
+        end
+        
+        if(dallanma_hata_i) begin
+        ps_duzeltme_aktif_r <= 1'b1;
+            if(guncelle_atladi_i) begin // hata var ve atlamasi gerekiyordu
+                if(dallanma_gecmis_imm_r[dg_hata_idx][11]) begin
+                    ps_duzeltme_r <= dallanma_gecmis_ps_r[dg_hata_idx] + {20'hFFFFF,dallanma_gecmis_imm_r[dg_hata_idx]};
+                end
+                else begin
+                    ps_duzeltme_r <= dallanma_gecmis_ps_r[dg_hata_idx] + {20'h00000,dallanma_gecmis_imm_r[dg_hata_idx]};
+                end
+            end
+            else begin  // hata var ve atlamamasi gerekiyordu
+                ps_duzeltme_r <= dallanma_gecmis_ps_r[dg_hata_idx];
+            end
+        end   
+        else begin
+        ps_duzeltme_aktif_r <= 1'b0;
+        ps_duzeltme_r <= 0;
+        end
+          
+        if(durum_r[an_str_idx][1]) begin // Atlar
+            dallanma_gecmis_imm_r[dg_idx] <= Br_imm;
+            dallanma_gecmis_ps_r[dg_idx] <= ps_i;
+            dg_idx <= dg_idx + 1'b1;  // 3->0
+        end
+        
 	end
 	else begin
 	    atlamaz_tahmin <= 0;
@@ -205,12 +260,23 @@ module Dallanma_Ongoru_Blogu(
         atlamadi <= 0;
         atladi <= 0;
         
+        dg_idx <= 0;
+        ps_duzeltme_r <= 0;
+        ps_duzeltme_aktif_r <= 0;
+        dg_hata_idx <= 0;
+        
 		for(i=0;i<128;i=i+1) begin
 			etiket_r[i] <= 0;
 			durum_r[i] <= 0;
 			hedef_adres_r[i] <= 0;
 		end
-	
+		
+		for(i=0 ; i<3 ; i=i+1) begin
+            dallanma_gecmis_imm_r[i] = 0;
+            dallanma_gecmis_ps_r[i] = 0;
+		end
+	   
+	   
 	end
 	
 	end
