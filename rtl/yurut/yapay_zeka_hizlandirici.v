08755d09c37f20791d0573d40ab2fa5de6e4a42c
ahmetthakan <113829539+ahmetthakan@users.noreply.github.com>
2022-12-28 17:11:41 +0300

    Post Synthesis Simulation +

--- rtl/yurut/yapay_zeka_hizlandirici.v
+++ rtl/yurut/yapay_zeka_hizlandirici.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-/*
+/*  POST SYNTHESIS SIMULATION'I GECTI
     or.
     bir cevrim 2 filtre yukle, hemen ustune conv yap gelirse, 1 cevrim fazladan beklenmesi gerekiyor
     bu durum cozulmeli (bir_cevrim_stall_o denetim birimine verilecek)
@@ -39,144 +39,243 @@ module yapay_zeka_hizlandirici(
     );
     
     reg conv_hazir_r = 0;
+	reg conv_hazir_r_next = 0;
+	
     reg [31:0] convolution_sonuc_r =0;
+	reg [31:0] convolution_sonuc_r_next =0;
+	
     reg bir_cevrim_stall_r = 0;
     reg bir_cevrim_stall_r_next = 0;
-    assign conv_hazir_o = conv_hazir_r;
-    assign convolution_sonuc_o = convolution_sonuc_r;
-    assign bir_cevrim_stall_o = bir_cevrim_stall_r;
+	
+    assign conv_hazir_o = blok_aktif_i ? conv_hazir_r : 0;
+    assign convolution_sonuc_o = blok_aktif_i ? convolution_sonuc_r : 0;
+    assign bir_cevrim_stall_o = blok_aktif_i ? bir_cevrim_stall_r : 0;
     
     reg [31:0] veri_matris_r [15:0];
     reg [31:0] filtre_matris_r [15:0];
+	reg [15:0] rs1_veri_r = 0;
+    reg [15:0] rs2_veri_r = 0;
+    reg [15:0] rs1_veri_r_next = 0;
+    reg [15:0] rs2_veri_r_next = 0;
    
     
     reg [3:0] veri_matris_idx = 0;
-    reg [3:0] filtre_matris_idx = 0;
+    reg [3:0] filtre_matris_idx = 0; 
+	reg [3:0] veri_matris_idx_next = 0;
+    reg [3:0] filtre_matris_idx_next = 0;
     
     reg [15:0] veri_matris_dolu = 0;
     reg [15:0] filtre_matris_dolu = 0;
+	reg [15:0] veri_matris_dolu_next = 0;
+    reg [15:0] filtre_matris_dolu_next = 0;
     wire [15:0] conva_hazir;
     assign conva_hazir = veri_matris_dolu & filtre_matris_dolu;
     
-    reg [31:0] conv_sonuc = 0;
     reg [3:0] conv_idx = 0;
-    reg [31:0] conv_sonuc_next = 0;
     reg [3:0] conv_idx_next = 0;
+    
+    localparam pasif = 3'b000;
+    localparam filtre_1 = 3'b001;
+    localparam filtre_2 = 3'b010;
+    localparam veri_1 = 3'b011;
+    localparam veri_2 = 3'b100;
+    
+    reg [2:0] mod = 0;
+    reg [2:0] mod_next = 0;
     
     integer i = 0;
     
-    always @ * begin
-    conv_sonuc_next = conv_sonuc;
-    conv_idx_next = conv_idx;
-    bir_cevrim_stall_r_next = 1'b0;
+    initial begin
+        for(i=0 ; i<16 ; i=i+1) begin
+            veri_matris_r[i] = 0;
+            filtre_matris_r[i] = 0;
+        end
     
-    if(blok_aktif_i) begin
-        if(conva_hazir[conv_idx]) begin
-            conv_sonuc_next = conv_sonuc + (veri_matris_r[conv_idx] * filtre_matris_r[conv_idx]);
-            conv_idx_next = conv_idx + 1'b1;  // 15->0
-        end
+    end
+    
+    
+    always @ * begin
+    if(rst_i) begin
         
-        if((conv_idx<15) && conv_yap_yaz_en_i && conva_hazir[conv_idx] && conva_hazir[conv_idx+1'b1]) begin
-                bir_cevrim_stall_r_next = 1'b1;    
-        end
-        else begin
-                bir_cevrim_stall_r_next = 1'b0;
-        end
+        bir_cevrim_stall_r_next     = 0;                         
+        conv_hazir_r_next           = 0;               
+        convolution_sonuc_r_next    = 0;        
+                      
+        veri_matris_idx_next        = 0;            
+        filtre_matris_idx_next      = 0;          
+                                
+        veri_matris_dolu_next       = 0;           
+        filtre_matris_dolu_next     = 0;         
+                                                     
+        conv_idx_next               = 0;                   
+                                      
+        mod_next                    = 0;                                      
+                                     
+        rs1_veri_r_next             = 0;                 
+        rs2_veri_r_next             = 0;                 
         
     end
-   
+    else begin
+    
+        bir_cevrim_stall_r_next = bir_cevrim_stall_r;
+        convolution_sonuc_r_next = convolution_sonuc_r;
+        
+        veri_matris_idx_next = veri_matris_idx;
+        filtre_matris_idx_next = filtre_matris_idx;
+        
+        veri_matris_dolu_next = veri_matris_dolu;
+        filtre_matris_dolu_next = filtre_matris_dolu;
+        
+        conv_idx_next = conv_idx;
+        
+        mod_next = 0;
+        conv_hazir_r_next = conv_hazir_r;
+        
+        rs1_veri_r_next = rs1_veri_r;
+        rs2_veri_r_next = rs2_veri_r;
+            
+            if(conv_idx == 15) begin 
+                conv_hazir_r_next = 1'b1;
+                bir_cevrim_stall_r_next = 1'b0;
+            end
+        
+            if(conv_yap_yaz_en_i) begin
+               if((conv_idx == 15) || conv_hazir_r) begin
+                    bir_cevrim_stall_r_next = 1'b0;
+                    conv_hazir_r_next = 1'b1;
+               end
+               else if(conva_hazir[conv_idx + 1'b1] == 0) begin
+                    bir_cevrim_stall_r_next = 1'b0;
+                    conv_hazir_r_next = 1'b1;
+               end
+               else begin
+                    bir_cevrim_stall_r_next = 1'b1;
+                    conv_hazir_r_next = 1'b0;
+               end
+            end
+            
+            if(conva_hazir[conv_idx] && (!conv_hazir_r)) begin
+                convolution_sonuc_r_next = convolution_sonuc_r + (veri_matris_r[conv_idx] * filtre_matris_r[conv_idx]);
+                conv_idx_next = conv_idx + 1'b1;  // 15->0
+            end
+            else begin
+                convolution_sonuc_r_next = convolution_sonuc_r;
+                conv_idx_next = conv_idx; 
+            end
+          
+            if(!conv_hazir_r) begin
+                if((!filtre_sil_i) && (!veri_sil_i)) begin
+                    
+                    if(filtre_rs1_en_i && (!filtre_rs2_en_i)) begin
+                        rs1_veri_r_next = rs1_veri_i;
+                        filtre_matris_dolu_next[filtre_matris_idx] = 1'b1;
+                        filtre_matris_idx_next = filtre_matris_idx + 1'b1;
+                        mod_next = filtre_1;
+                    end
+                    
+                    else if(filtre_rs2_en_i) begin
+                        rs2_veri_r_next = rs2_veri_i; // rs2 yükleniyorsa zaten rs1 kesinlikle yükelenecek
+                        filtre_matris_dolu_next[filtre_matris_idx + 1'b1] = 1'b1;
+                        filtre_matris_dolu_next[filtre_matris_idx] = 1'b1;
+                        filtre_matris_idx_next = filtre_matris_idx + 2'b10;
+                        mod_next = filtre_2;
+                    end 
+                   
+                    else begin
+                        if(veri_rs1_en_i && (!veri_rs2_en_i)) begin
+                            rs1_veri_r_next = rs1_veri_i;
+                            veri_matris_dolu_next[veri_matris_idx] = 1'b1;
+                            veri_matris_idx_next = veri_matris_idx + 1'b1;
+                            mod_next = veri_1;
+                        end
+                        
+                        else if(veri_rs2_en_i) begin
+                            rs2_veri_r_next = rs2_veri_i; // rs2 yukleniyorsa zaten rs1 kesinlikle yuklenecek
+                            veri_matris_dolu_next[veri_matris_idx + 1'b1] = 1'b1;
+                            veri_matris_dolu_next[veri_matris_idx] = 1'b1;
+                            veri_matris_idx_next = veri_matris_idx + 2'b10;
+                            mod_next = veri_2;
+                        end
+                    end
+                   
+                
+               end
+           end
+    end
     end
     
     always @ (posedge clk_i) begin
-    
-    // matrisleri doldurma
-    
-    if(rst_i) begin
-        conv_sonuc <= 0;        
-        conv_idx <= 0;          
-        convolution_sonuc_r <= 0; 
-        filtre_matris_idx <= 0; 
-        filtre_matris_dolu <= 0;
-        veri_matris_idx <= 0;
-        veri_matris_dolu <= 0;
-        conv_hazir_r <= 0;
-        bir_cevrim_stall_r <= 0;
+        if(filtre_sil_i || veri_sil_i) begin
+         rs1_veri_r <= 0;
+         rs2_veri_r <= 0;
+         convolution_sonuc_r <= 0;
+         conv_idx <= 0; 
+         conv_hazir_r <= 0;
+         bir_cevrim_stall_r <= 0;
         
-        for(i=0 ; i<16 ; i=i+1) begin
-                filtre_matris_r[i] <= 0;
-                veri_matris_r[i] <= 0;
-        end
-        
-    end
-    else if(blok_aktif_i && !rst_i) begin
-    
-        if(filtre_sil_i) begin
-            for(i=0 ; i<16 ; i=i+1) begin
-                filtre_matris_r[i] <= 0;
-            end
-            conv_sonuc <= 0;
-            conv_idx <= 0;
-            convolution_sonuc_r <= 0;
-            filtre_matris_idx <= 0;
-            filtre_matris_dolu <= 0;
-            conv_hazir_r <= 0;
-        end
-        
-        if(veri_sil_i) begin
-            for(i=0 ; i<16 ; i=i+1) begin
-                veri_matris_r[i] <= 0;
-            end
-            conv_sonuc <= 0;
-            conv_idx <= 0;
-            convolution_sonuc_r <= 0;
-            veri_matris_idx <= 0;
-            veri_matris_dolu <= 0;
-            conv_hazir_r <= 0;
-        end
-        
-        if(!filtre_sil_i && !veri_sil_i) begin
-            conv_sonuc <= conv_sonuc_next;
-            conv_idx <= conv_idx_next;
-            bir_cevrim_stall_r <= bir_cevrim_stall_r_next;
-            
-            if(conv_yap_yaz_en_i) begin
-                convolution_sonuc_r <= conv_sonuc_next; // bu next olmayabilir *
-                conv_hazir_r <= 1'b1;
-            end
-            else begin
-                convolution_sonuc_r <= 0;
-                conv_hazir_r <= 1'b0;
+            if(filtre_sil_i) begin
+                for(i=0 ; i<16 ; i=i+1) begin
+                    filtre_matris_r[i] <= 0;
+                end
+                filtre_matris_idx <= 0;
+                filtre_matris_dolu <= 0;
+                veri_matris_idx <= veri_matris_idx_next;
+                veri_matris_dolu <= veri_matris_dolu_next;
             end
             
-            if(filtre_rs1_en_i) begin
-                filtre_matris_r[filtre_matris_idx] <= rs1_veri_i;
-                filtre_matris_dolu[filtre_matris_idx] <= 1'b1;
-            end
-            
-            if(filtre_rs2_en_i) begin
-                filtre_matris_r[filtre_matris_idx + 1'b1] <= rs2_veri_i; // rs2 yükleniyorsa zaten rs1 kesinlikle yükelenecek
-                filtre_matris_dolu[filtre_matris_idx + 1'b1] <= 1'b1;
-                filtre_matris_idx <= filtre_matris_idx + 2;
-            end 
-            else begin // yalnizca rs1 icin yuklendi
-                filtre_matris_idx <= filtre_matris_idx + 1;
-            end
-            
-            if(veri_rs1_en_i) begin
-                veri_matris_r[veri_matris_idx] <= rs1_veri_i;
-                veri_matris_dolu[veri_matris_idx] <= 1'b1;
-            end
-            
-            if(veri_rs2_en_i) begin
-                veri_matris_r[veri_matris_idx + 1'b1] <= rs2_veri_i; // rs2 yukleniyorsa zaten rs1 kesinlikle yuklenecek
-                veri_matris_dolu[veri_matris_idx + 1'b1] <= 1'b1;
-                veri_matris_idx <= veri_matris_idx + 2;
-            end
-            else begin // yalnizca rs1 icin yuklendi
-                veri_matris_idx <= veri_matris_idx + 1;
+            if(veri_sil_i) begin
+                for(i=0 ; i<16 ; i=i+1) begin
+                    veri_matris_r[i] <= 0;
+                end
+                veri_matris_idx <= 0;
+                veri_matris_dolu <= 0;
+                filtre_matris_idx <= filtre_matris_idx_next;
+                filtre_matris_dolu <= filtre_matris_dolu_next;
             end
         end
-    end
+	
+        else begin
+        
+        conv_hazir_r <= conv_hazir_r_next;
+        convolution_sonuc_r <= convolution_sonuc_r_next;
+        bir_cevrim_stall_r <= bir_cevrim_stall_r_next;
+        
+        veri_matris_idx <= veri_matris_idx_next;
+        filtre_matris_idx <= filtre_matris_idx_next;
+        
+        veri_matris_dolu <= veri_matris_dolu_next;
+        filtre_matris_dolu <= filtre_matris_dolu_next;
+        
+        conv_idx <= conv_idx_next;
+        
+        mod <= mod_next;
+        
+        rs1_veri_r <= rs1_veri_i;
+        rs2_veri_r <= rs2_veri_i;
+	
+	 
+	    case(mod_next)
+	       filtre_1 : begin
+	           filtre_matris_r[filtre_matris_idx] <= rs1_veri_r_next;
+	       end
+	       
+	       filtre_2 : begin
+	           filtre_matris_r[filtre_matris_idx] <= rs1_veri_r_next;
+			   filtre_matris_r[filtre_matris_idx + 1'b1] <= rs2_veri_r_next;
+	       end
+	       
+	       veri_1 : begin
+	           veri_matris_r[veri_matris_idx] <= rs1_veri_r_next;
+	       end
+	       
+	       veri_2 : begin
+	           veri_matris_r[veri_matris_idx] <= rs1_veri_r_next;
+               veri_matris_r[veri_matris_idx + 1'b1] <= rs2_veri_r_next;
+	       end
+		endcase
+	end
+	
+	
     
     end
     
