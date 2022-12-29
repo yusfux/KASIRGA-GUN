3037aa16623b9fe29ad6117d00f8d34cfc3f718e
nevavrl <104263590+nevavrl@users.noreply.github.com>
2022-12-29 17:01:31 +0300

    Add files via upload

--- rtl/getir/buyruk_onbellegi_denetleyicisi.v
+++ rtl/getir/buyruk_onbellegi_denetleyicisi.v
@@ -1,18 +1,18 @@
 module buyruk_onbellegi_denetleyicisi(
     input          clk                    ,
     input          rst                    ,// active low reset
-    input  [31:0]  adres_g                ,//anabellege iletilecek adres-psden alinir
-    input          onbellek_veri_bulundu_g, 
-    input          onbellek_bitti_g       ,//onbellekte islem tamamlandi
-    input  [31:0]  onbellek_veri_g        ,
-    input          anabellek_gecerli_obek_g,//** anabellekten gelen veri hazir
-    input  [127:0] anabellek_obek_g       , //**
-    output [127:0] onbellek_obek_c        , 
-    output         onbellege_obegi_yaz_c  ,
-    output [31:0]  anabellek_adres_c      , //anabellege verilen adres
-    output         anabellek_istek_c      , // anabellekten veri(buyruk) obegi almak icin anabellege yapilan istek
-    output [31:0]  veri_c                 ,  //on cozucuye iletilir
-    output         veri_hazir_c              //denetleyici buyrugu hazirladi
+    input  [31:0]  adres_i                ,//anabellege iletilecek adres-psden alinir
+    input          onbellek_veri_bulundu_i, 
+    input          onbellek_bitti_i       ,//onbellekte islem tamamlandi
+    input  [31:0]  onbellek_veri_i        ,
+    input          anabellek_gecerli_obek_i,//** anabellekten gelen veri hazir
+    input  [127:0] anabellek_obek_i       , //**
+    output [127:0] onbellek_obek_o        , 
+    output         onbellege_obegi_yaz_o  ,
+    output [31:0]  anabellek_adres_o      , //anabellege verilen adres
+    output         anabellek_istek_o      , // anabellekten veri(buyruk) obegi almak icin anabellege yapilan istek
+    output [31:0]  veri_o                 ,  //on cozucuye iletilir
+    output         veri_hazir_o              //denetleyici buyrugu hazirladi
 );
     localparam ONBELLEK  = 1'b0;
     localparam ANABELLEK = 1'b1;
@@ -33,12 +33,12 @@ module buyruk_onbellegi_denetleyicisi(
     reg         onbellege_obegi_yaz_ns;   
     reg [1:0] veri_araligi; // 00 01 10 11
     
-    assign anabellek_adres_c = anabellek_adres_r;
-    assign anabellek_istek_c = anabellek_istek_r;
-    assign veri_c            = veri_r           ;
-    assign veri_hazir_c      = veri_hazir_r     ;
-    assign onbellek_obek_c   = onbellek_obek_r  ;
-    assign onbellege_obegi_yaz_c = onbellege_obegi_yaz_r;
+    assign anabellek_adres_o = anabellek_adres_r;
+    assign anabellek_istek_o = anabellek_istek_r;
+    assign veri_o            = veri_r           ;
+    assign veri_hazir_o      = veri_hazir_r     ;
+    assign onbellek_obek_o   = onbellek_obek_r  ;
+    assign onbellege_obegi_yaz_o = onbellege_obegi_yaz_r;
     
     always @(posedge clk) begin
         if(rst)begin
@@ -72,16 +72,16 @@ module buyruk_onbellegi_denetleyicisi(
         onbellege_obegi_yaz_ns = 1'b0;
         case (durum)
             ONBELLEK : begin
-                if(onbellek_bitti_g) begin
-                    if(onbellek_veri_bulundu_g) begin
-                        veri_ns            = onbellek_veri_g;
+                if(onbellek_bitti_i) begin
+                    if(onbellek_veri_bulundu_i) begin
+                        veri_ns            = onbellek_veri_i;
                         veri_hazir_ns      = 1'b1;
                         anabellek_istek_ns = 1'b0;
                         durum_next      = ONBELLEK;
                     end
                     else begin              
-                        veri_araligi = adres_g[3:2];            
-                        anabellek_adres_ns = {adres_g[31:4], 4'b0000};// anabellege istek sinyali ayr?ca eklenebilir
+                        veri_araligi = adres_i[3:2];            
+                        anabellek_adres_ns = {adres_i[31:4], 4'b0000};// anabellege istek sinyali ayr?ca eklenebilir
                         anabellek_istek_ns = 1'b1;// anabellek sadece bu sinyal 1 oldugundaa istenilen adrese bakar
                         durum_next      = ANABELLEK;
                     end
@@ -90,22 +90,22 @@ module buyruk_onbellegi_denetleyicisi(
                   
             end
             ANABELLEK : begin
-                if(anabellek_gecerli_obek_g) begin
+                if(anabellek_gecerli_obek_i) begin
                     if(veri_araligi == 2'b00)begin
-                        veri_ns = anabellek_obek_g[31:0];
+                        veri_ns = anabellek_obek_i[31:0];
                     end
                     else if(veri_araligi == 2'b01)begin
-                        veri_ns = anabellek_obek_g[63:32];
+                        veri_ns = anabellek_obek_i[63:32];
                     end
                     else if(veri_araligi == 2'b10)begin
-                        veri_ns = anabellek_obek_g[95:64];
+                        veri_ns = anabellek_obek_i[95:64];
                     end
                     else if(veri_araligi == 2'b11)begin
-                        veri_ns = anabellek_obek_g[127:96];
+                        veri_ns = anabellek_obek_i[127:96];
                     end
                     veri_hazir_ns       = 1'b1            ;
                     anabellek_istek_ns  = 1'b0            ;         
-                    onbellek_obek_ns    = anabellek_obek_g;
+                    onbellek_obek_ns    = anabellek_obek_i;
                     onbellege_obegi_yaz_ns = 1'b1         ;
                     durum_next          = ONBELLEK        ;
                 end
