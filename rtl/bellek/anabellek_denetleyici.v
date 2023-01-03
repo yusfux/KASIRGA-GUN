12829a1495bbf2ba590da3048d04152b3b3acb8d
nevavrl <104263590+nevavrl@users.noreply.github.com>
2023-01-03 23:39:37 +0300

    Add files via upload

--- rtl/bellek/anabellek_denetleyici.v
+++ rtl/bellek/anabellek_denetleyici.v
@@ -50,88 +50,72 @@ module anabellek_denetleyici(
     reg        iomem_valid_r;
     reg        iomem_valid_ns;
     
+    reg        anabellek_musait_r;
+    reg        anabellek_musait_ns;
+    
     assign okunan_veri_obegi_o = okunan_veri_obegi_r;
-    assign okunan_veri_obegi_hazir_o = okunan_veri_obegi_hazir_r;
+    //assign okunan_veri_obegi_hazir_o = okunan_veri_obegi_hazir_r;
     assign adres_o = adres_r; //adres_r yapinca yanlis adres gonderiyor
     assign yaz_veri_o = yaz_veri_r;
     assign wr_strb_o = wr_strb_r;
-    assign iomem_valid_o = iomem_valid_r;
-    assign anabellek_musait_o = ((durum == MUSAIT)&&(durum_next == MUSAIT)) || ((durum == OKU)&&(durum_next == MUSAIT)) || ((durum == YAZ)&&(durum_next == MUSAIT));
-    
-    always @(posedge clk_i) begin
-        if(rst_i) begin
-            durum <= durum_next;
-            okunan_veri_obegi_hazir_r <= okunan_veri_obegi_hazir_ns;
-            okunan_veri_obegi_r <= okunan_veri_obegi_ns;
-            adres_r <= adres_ns;
-            veri_sayisi_r <= veri_sayisi_ns;
-            yaz_veri_r <= yaz_veri_ns;
-            iomem_valid_r <= iomem_valid_ns;
-            wr_strb_r <= wr_strb_ns;
-        end
-        else begin
-            durum <= MUSAIT;
-            veri_sayisi_r <= 3'd0;
-            okunan_veri_obegi_hazir_r <= 0;
-            okunan_veri_obegi_r <= 0;
-            adres_r <= 0;
-            yaz_veri_r <= 0;
-            wr_strb_r <= 0; 
-            iomem_valid_r <= 0; 
-        end
-    
-    end
+    assign iomem_valid_o = iomem_valid_r;//!(durum_next == MUSAIT);
+    assign anabellek_musait_o = anabellek_musait_r;//(durum_next == MUSAIT);
+    assign okunan_veri_obegi_hazir_o = okunan_veri_obegi_hazir_r; //(durum == OKU) && (durum_next == MUSAIT);
+
     always @* begin
-        durum_next  = durum;
-        okunan_veri_obegi_hazir_ns = 0;
+        durum_next = durum;
         okunan_veri_obegi_ns = okunan_veri_obegi_r;
+        okunan_veri_obegi_hazir_ns = okunan_veri_obegi_hazir_r;
         adres_ns = adres_r;
         veri_sayisi_ns = veri_sayisi_r;
         yaz_veri_ns = yaz_veri_r;
         iomem_valid_ns = iomem_valid_r;
         wr_strb_ns = wr_strb_r;
+        anabellek_musait_ns = anabellek_musait_r;
         case(durum)
-            MUSAIT : begin
+            MUSAIT: begin
             iomem_valid_ns = 1'b0;
+            anabellek_musait_ns = 1'b1;
                 if(anabellege_istek_i)begin
                     if(oku_i)begin
                         adres_ns = oku_adres_i;
-                        //adres_ns = adres_r + 4;
                         iomem_valid_ns = 1;
                         wr_strb_ns = 4'b0000;
+                        anabellek_musait_ns = 1'b0;
                         durum_next = OKU;
                     end
                     else if(yaz_i)begin
                         adres_ns = yaz_adres_i;
-                        //adres_ns = adres_r + 4;
                         wr_strb_ns = 4'b1111;
                         yaz_veri_ns = yaz_veri_obegi_i[31:0];
                         iomem_valid_ns = 1;
+                        anabellek_musait_ns = 1'b0;
                         durum_next = YAZ;
                     end
                     
                 end
             end
-            YAZ : begin
+            YAZ: begin
                 if(iomem_ready_i) begin 
                      veri_sayisi_ns = veri_sayisi_r + 1;
                      wr_strb_ns = 4'b1111;
                      iomem_valid_ns = 1'b1;
-                     if(veri_sayisi_ns == 3'd1)begin
+                     if(veri_sayisi_r == 3'd0)begin
                         yaz_veri_ns = yaz_veri_obegi_i[63:32];
                         adres_ns = adres_r + 4;
                      end
-                     else if(veri_sayisi_ns == 3'd2) begin
+                     else if(veri_sayisi_r == 3'd1) begin
                         yaz_veri_ns = yaz_veri_obegi_i[95:64];
                         adres_ns = adres_r + 4;
                      end
-                     else if(veri_sayisi_ns == 3'd3) begin
+                     else if(veri_sayisi_r == 3'd2) begin
                         yaz_veri_ns = yaz_veri_obegi_i[127:96];
                         adres_ns = adres_r + 4;
                      end
-                     else if(veri_sayisi_ns == 3'd4)begin
+                     else if(veri_sayisi_r == 3'd3)begin
                         veri_sayisi_ns = 0;
-                        //iomem_valid_ns = 0; musait durumunda 0laniyor
+                        iomem_valid_ns = 0; 
+                        anabellek_musait_ns = 1'b1;
                         durum_next = MUSAIT;
                      end
                 end
@@ -141,10 +125,11 @@ module anabellek_denetleyici(
                      veri_sayisi_ns = veri_sayisi_r + 1;
                      okunan_veri_obegi_ns = okunan_veri_obegi_r << 32;
                      okunan_veri_obegi_ns[31:0] = anabellekten_veri_i;
-                     if(veri_sayisi_ns == 3'd4)begin
-                        okunan_veri_obegi_hazir_ns = 1'b1;
+                     if(veri_sayisi_r == 3'd3)begin
                         veri_sayisi_ns = 0;
                         iomem_valid_ns = 0;
+                        anabellek_musait_ns = 1'b1;
+                        okunan_veri_obegi_hazir_ns = 1'b1;
                         durum_next = MUSAIT;
                      end
                      else begin 
@@ -154,9 +139,34 @@ module anabellek_denetleyici(
                      end
                 end
             end
-            
         endcase
-        
+    end
+
+    always @(posedge clk_i) begin
+        if(!rst_i) begin
+            durum <= MUSAIT;
+            veri_sayisi_r <= 3'd0;
+            okunan_veri_obegi_r <= 128'd0;
+            adres_r <= 32'd0;
+            yaz_veri_r <= 32'd0;// multi driven pin warning, satiri acma
+            wr_strb_r <= 4'd0; //multi driven pin warning, satiri acma
+            iomem_valid_r <= 1'b0; //multi driven pin warning, satiri acma
+            anabellek_musait_r <= 1'b0;
+            okunan_veri_obegi_hazir_r <= 1'b0;
+        end
+        else begin
+            durum <= durum_next;
+            anabellek_musait_r <= anabellek_musait_ns;
+            okunan_veri_obegi_r <= okunan_veri_obegi_ns;
+            adres_r <= adres_ns;
+            veri_sayisi_r <= veri_sayisi_ns;
+            yaz_veri_r <= yaz_veri_ns;
+            iomem_valid_r <= iomem_valid_ns;
+            wr_strb_r <= wr_strb_ns;
+            okunan_veri_obegi_hazir_r <= okunan_veri_obegi_hazir_ns;
+        end
+    
     end
     
 endmodule
+    
