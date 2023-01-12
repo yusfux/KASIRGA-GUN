07d190d189879cf70dbf5ec0f8d33e702f169d33
nevavrl <104263590+nevavrl@users.noreply.github.com>
2023-01-13 01:06:57 +0300

    Add files via upload

--- rtl/getir/buyruk_onbellegi.v
+++ rtl/getir/buyruk_onbellegi.v
@@ -2,102 +2,82 @@
 
 module buyruk_onbellegi(
     input               clk_i,
-    input               rst_i,                      //  active low reset
+    input               rst_i,
     
-    input     [31:0]    adres_i,                    //  bellek_adresi !(anabellek denetleyicisi ve onbellege adres olarak ayni wire (program sayaci) baglanacak)
+    input     [31:0]    adres_i,                    //  bellek_adresi
     input     [127:0]   buyruk_obegi_i,             //  bellekten gelen öbek
-    input               anabellekten_obek_geldi_i,  //  bellekten öbek gelme islemi tamamlandi
+    input               anabellekten_obek_geldi_i,   //  bellekten öbek gelme i?lemi tamamland?
         
     output    [31:0]    buyruk_o,                   //  buyruk
-    output              adres_bulundu_o             //  önbellekte adres bulundu          
+    output              adres_bulundu_o            //  önbellekte adres bulundu          
 );
  
-reg  [31:0]  buyruk_r       = 32'd0;         
-reg          adres_bulundu_r = 1'b0;  
+reg     [31:0]      buyruk_r = 32'd0;         
+reg                 adres_bulundu_r = 1'b0;  
                                               
-reg  [127:0] onbellek       [127:0]; 
-reg  [19:0]  etiket_buffer  [127:0];  
-reg          gecerli_buffer [127:0];  
+reg     [127:0]     onbellek        [127:0];  
+reg     [19:0]      etiket_buffer   [127:0];  
+reg                 gecerli_buffer  [127:0];  
 
-wire [7:0]   onbellek_adres;    
-wire [1:0]   secilen_byte;          
-wire [19:0]  etiket;            
+wire     [7:0]      onbellek_adres;    
+wire     [1:0]      secilen_byte;          
+wire     [19:0]     etiket;            
 
 integer i;
-initial begin
-    for(i=0 ; i<=255 ; i= i+1) begin
-        onbellek[i]       = 128'b0;
-        etiket_buffer[i]  = 20'b0;
-        gecerli_buffer[i] = 1'b0;
-    end
-end
- 
-assign  etiket          = adres_i[31:12];
-assign  onbellek_adres  = adres_i[11:4] ; 
-assign  secilen_byte    = adres_i[3:2]  ;
 
+assign  onbellek_adres      =    adres_i[11:4]; 
+assign  secilen_byte        =    adres_i[3:2];
+assign  etiket              =    adres_i[31:12];
 
 always @(posedge clk_i) begin
-    if(!rst_i)    begin
-        buyruk_r        = 32'd0;
-        adres_bulundu_r = 32'd0;
-        for(i=0 ; i<=255 ; i= i+1) begin
-            onbellek[i]       = 128'b0;
-            etiket_buffer[i]  = 20'b0;
+    if(!rst_i)begin
+        buyruk_r <= 32'd0;      
+        adres_bulundu_r <= 1'b0;
+                           
+        for(i=0 ; i<=127 ; i= i+1) begin
+            onbellek[i] = 128'b0;
+            etiket_buffer[i] = 20'b0;
             gecerli_buffer[i] = 1'b0;
-        end    
+        end                                      
     end
     else begin
-            if(anabellekten_obek_geldi_i) begin
-                onbellek[onbellek_adres] <= buyruk_obegi_i;
-                adres_bulundu_r          <= 0 ; 
-            end 
-    end
-end
-
-always @(posedge clk_i) begin
-    if(!rst_i)    begin
-        buyruk_r        = 32'd0;
-        adres_bulundu_r = 32'd0;
-        for(i=0 ; i<=255 ; i= i+1) begin
-            onbellek[i]       = 128'b0;
-            etiket_buffer[i]  = 20'b0;
-            gecerli_buffer[i] = 1'b0;
-        end
-    end
-    else begin   
          
-            if(gecerli_buffer[onbellek_adres] == 1'b0) begin
-                adres_bulundu_r                <= 1'b0;
-                etiket_buffer[onbellek_adres]  <= etiket;
-                gecerli_buffer[onbellek_adres] <= 1'b1;
-            end        
-    
-            else if(etiket_buffer[onbellek_adres] != etiket) begin 
-                adres_bulundu_r <= 0;
-                etiket_buffer[onbellek_adres] <= etiket;
-            end
-            else begin
-                case(secilen_byte) 
-                    2'd0: begin
-                         buyruk_r <= onbellek [onbellek_adres][31 : 0];         
-                    end
-                    2'd1 : begin
-                         buyruk_r <= onbellek [onbellek_adres][63 : 32]; 
-                    end 
-                    2'd2 : begin
-                        buyruk_r  <= onbellek [onbellek_adres][95 : 64];
-                    end 
-                    2'd3 : begin
-                        buyruk_r  <= onbellek [onbellek_adres][127 : 96];
-                    end  
-                endcase
-                adres_bulundu_r <= 1;
-            end
+        if(gecerli_buffer[onbellek_adres] == 1'b0) begin
+            adres_bulundu_r <= 1'b0;
+            etiket_buffer[onbellek_adres] <= etiket;
+            gecerli_buffer[onbellek_adres] <= 1'b1;
+        end        
+
+        else if(etiket_buffer[onbellek_adres] != etiket) begin 
+            adres_bulundu_r <= 0;
+            etiket_buffer[onbellek_adres]  <=  etiket;
+        end
+
+        else if(anabellekten_obek_geldi_i) begin
+            onbellek[onbellek_adres] <= buyruk_obegi_i;
+        end             
+        
+        else begin
+            case(secilen_byte) 
+                2'd0: begin
+                     buyruk_r  <=  onbellek [onbellek_adres][31 : 0];         
+                end
+                2'd1 : begin
+                     buyruk_r  <=  onbellek [onbellek_adres][63 : 32]; 
+                end 
+                2'd2 : begin
+                    buyruk_r  <=  onbellek [onbellek_adres][95 : 64];
+                end 
+                2'd3 : begin
+                    buyruk_r  <=  onbellek [onbellek_adres][127 : 96];
+                end  
+            endcase
+            adres_bulundu_r  <= 1;
+        end
     end
 end        
           
-assign buyruk_o        = buyruk_r;         
-assign adres_bulundu_o = adres_bulundu_r;  
+assign   buyruk_o = buyruk_r;         
+assign   adres_bulundu_o = adres_bulundu_r;  
 
 endmodule
--- rtl/getir/buyruk_onbellegi_denetleyici.v
+++ rtl/getir/buyruk_onbellegi_denetleyici.v
@@ -18,7 +18,7 @@ module buyruk_onbellegi_denetleyici(
     // anabellek denetleyici sinyalleri
     
     input               anabellek_musait_i,
-    input               getir_asamasina_veri_hazir_i,
+    input               getir_asamasina_veri_hazir_i,// anabellekten obek geldi sinyali
     input   [127:0]     okunan_obek_i,
     
     output  [31:0]      anabellek_adres_o,
@@ -46,8 +46,10 @@ module buyruk_onbellegi_denetleyici(
     reg     [1:0]       durum_ns;
     
     
-    wire [1:0] veri_araligi;
-    assign veri_araligi = adres_i[3:2];
+    reg [1:0] veri_araligi_r;
+    reg [1:0] veri_araligi_ns;
+    
+
       
     always @(*) begin
         durum_ns             = durum_r;
@@ -56,6 +58,7 @@ module buyruk_onbellegi_denetleyici(
         veri_obegi_r         = 128'd0;
         onbellege_obek_yaz_r = 1'b0; 
         buyruk_hazir_r       = 1'b0; 
+        veri_araligi_ns       = veri_araligi_r;
             
         if(!durdur_i)begin   
             case(durum_r)
@@ -71,6 +74,7 @@ module buyruk_onbellegi_denetleyici(
                         if(anabellek_musait_i) begin
                             anabellek_adres_r = {adres_i[31:4], 4'b0000};
                             buyruk_hazir_r    = 1'b0;
+                            veri_araligi_ns = adres_i[3:2];
                             durum_ns          = ANABELLEK_OKU ;                     
                         end
                     end
@@ -78,7 +82,7 @@ module buyruk_onbellegi_denetleyici(
                 
                 ANABELLEK_OKU :begin
                      if(getir_asamasina_veri_hazir_i) begin
-                          
+                          /*
                                 if(veri_araligi == 2'b00)begin
                                     buyruk_r = okunan_obek_i[31:0];
                                 end
@@ -91,9 +95,10 @@ module buyruk_onbellegi_denetleyici(
                                 else if(veri_araligi == 2'b11)begin
                                     buyruk_r = okunan_obek_i[127:96];
                                 end
-                                
+                               
                                 buyruk_hazir_r = 1'b1;
-                                                        
+                             */ // BU ISLEMLER ANABELLEK OKUDAN ANABELLEK YAZ DURUMUNA EKLENDI  
+                                // SEBEBI, buyruk hazir onbellege verinin yaz?ld?g? cevrimde '1' yapmak. 1 cevrim erken '1' oldugunda onbellege veri yazilacakken yeni adres gelebilir, eger gelirse onbellege yazilmiyor                     
                                 onbellege_obek_yaz_r = 1'b1;
                                 veri_obegi_r         = okunan_obek_i;
                                 durum_ns             = ONBELLEK_YAZ;    
@@ -101,6 +106,20 @@ module buyruk_onbellegi_denetleyici(
             
                 end
                 ONBELLEK_YAZ : begin 
+                    if(veri_araligi_r == 2'b00)begin
+                        buyruk_r = okunan_obek_i[31:0];
+                    end
+                    else if(veri_araligi_r == 2'b01)begin
+                        buyruk_r = okunan_obek_i[63:32];
+                    end
+                    else if(veri_araligi_r == 2'b10)begin
+                        buyruk_r = okunan_obek_i[95:64];
+                    end
+                    else if(veri_araligi_r == 2'b11)begin
+                        buyruk_r = okunan_obek_i[127:96];
+                    end
+                    
+                    buyruk_hazir_r = 1'b1;
                     durum_ns = ONBELLEK_OKU;
                     
                 end
@@ -114,7 +133,8 @@ module buyruk_onbellegi_denetleyici(
            durum_r <= ONBELLEK_OKU;
        end
        else begin
-           durum_r <= durum_ns;
+           durum_r        <= durum_ns;
+           veri_araligi_r <= veri_araligi_ns;
        end
    end
     
