da04a3eb494931f6c3dc2a8f2257fe5f56588203
Ahmet Hakan YILDIZ <ahmethknyildiz@gmail.com>
2023-03-31 17:48:48 +0300

    sorunlar duzeltildi

--- FPGA/yapay_zeka_hizlandirici.v
+++ FPGA/yapay_zeka_hizlandirici.v
@@ -45,10 +45,13 @@ module yapay_zeka_hizlandirici(
    reg bir_cevrim_stall_r_next;
    
    reg conv_yap_en_i_r;
+      
+   reg cikis_ver;
+   reg cikis_ver_next;
    
-   assign conv_hazir_o = (conv_yap_en_i_r && conv_hazir_r); // stall_o durumunda da calismali
+   assign conv_hazir_o = (conv_yap_en_i_r && conv_hazir_r) || (bir_cevrim_stall_r && conv_hazir_r_next) || (cikis_ver); // stall_o durumunda da calismali
    assign convolution_sonuc_o =  convolution_sonuc_r;
-   assign stall_o = bir_cevrim_stall_r; // blok_aktif_i ? bir_cevrim_stall_r : 0 seklinde yapmistim
+   assign stall_o = bir_cevrim_stall_r_next; // blok_aktif_i ? bir_cevrim_stall_r : 0 seklinde yapmistim
    
    reg [31:0] veri_matris_r [15:0]; 
    reg [31:0] filtre_matris_r [15:0];
@@ -70,7 +73,13 @@ module yapay_zeka_hizlandirici(
    
    reg [3:0] conv_idx;
    reg [3:0] conv_idx_next;
-    
+   
+   reg toplama_islemi;
+   reg toplama_islemi_next;
+   
+   reg [31:0] carpim_sonuc;
+   reg [31:0] carpim_sonuc_next;
+   
    localparam pasif = 3'b000;
    localparam filtre_1 = 3'b001;
    localparam filtre_2 = 3'b010;
@@ -79,6 +88,7 @@ module yapay_zeka_hizlandirici(
    
    reg [2:0] mod;
    reg [2:0] mod_next; 
+
     
     always @ * begin
        if(!rst_i) begin // rst_i 0 ise reset
@@ -92,7 +102,10 @@ module yapay_zeka_hizlandirici(
          conv_idx_next               = 4'd0;                                                   
          mod_next                    = 3'd0;                                                             
          rs1_veri_r_next             = 32'd0;                 
-         rs2_veri_r_next             = 32'd0;                 
+         rs2_veri_r_next             = 32'd0;  
+         toplama_islemi_next         = 1'b0;         
+         carpim_sonuc_next           = 32'd0;
+         cikis_ver_next              = 1'b0;
            
        end
        else begin
@@ -111,32 +124,28 @@ module yapay_zeka_hizlandirici(
          mod_next = 3'd0;
          rs1_veri_r_next = 32'd0;
          rs2_veri_r_next = 32'd0;
+         cikis_ver_next  = 1'b0;
          
          conv_hazir_r_next = conv_hazir_r;
          
+         toplama_islemi_next = toplama_islemi;
+         carpim_sonuc_next   = carpim_sonuc;
          
-         if(durdur_i) begin // conv hazir degilse wrapper'da sonuc aktarilmiyor, ama icerideki sinyaller ayni kalicak
-            conv_hazir_r_next = 1'b0; 
-         end
-         else begin
+         if(!durdur_i) begin
          
-            if(conv_yap_en_i_r && conv_hazir_r)
-               conv_hazir_r_next = 1'b0;
-            else 
-               
-            if(conv_idx == 15) begin 
-               conv_hazir_r_next = 1'b1;
-               bir_cevrim_stall_r_next = 1'b0;
-            end
-            
-            if(conv_yap_en_i) begin
-               if((conv_idx == 15) || conv_hazir_r) begin
+//            if(conv_idx == 15) begin 
+//               conv_hazir_r_next = 1'b1;
+//               bir_cevrim_stall_r_next = 1'b0;
+//            end
+
+            if(conv_yap_en_i) begin // 
+               if(conv_hazir_r) begin
                   bir_cevrim_stall_r_next = 1'b0;
                   conv_hazir_r_next = 1'b1;
                end
                else if(conva_hazir[conv_idx + 1'b1] == 1'b0) begin
                   bir_cevrim_stall_r_next = 1'b0;
-                  conv_hazir_r_next = 1'b1;
+                  cikis_ver_next = 1'b1;
                end
                else begin
                   bir_cevrim_stall_r_next = 1'b1;
@@ -144,13 +153,22 @@ module yapay_zeka_hizlandirici(
                end
             end
             
-            if(conva_hazir[conv_idx] && (!conv_hazir_r)) begin
-               convolution_sonuc_r_next = convolution_sonuc_r + (veri_matris_r[conv_idx] * filtre_matris_r[conv_idx]);
+            
+            // sonra topla
+            if(toplama_islemi) begin
+               convolution_sonuc_r_next = convolution_sonuc_r + carpim_sonuc;
                conv_idx_next = conv_idx + 1'b1;  // 15->0
+               toplama_islemi_next = 1'b0;
+               
+               if(conv_idx == 15) begin
+                  conv_hazir_r_next = 1'b1;
+                  bir_cevrim_stall_r_next = 1'b0;
+               end
             end
-            else begin
-               convolution_sonuc_r_next = convolution_sonuc_r;
-               conv_idx_next = conv_idx; 
+            // once carp
+            else if(conva_hazir[conv_idx] && (!conv_hazir_r)) begin
+               carpim_sonuc_next   = veri_matris_r[conv_idx] * filtre_matris_r[conv_idx];
+               toplama_islemi_next = 1'b1;
             end
             
             if(!conv_hazir_r) begin
@@ -163,7 +181,7 @@ module yapay_zeka_hizlandirici(
                   end
                   else if(filtre_rs2_en_i) begin
                      rs1_veri_r_next = rs1_veri_i;
-                     rs2_veri_r_next = rs2_veri_i; // rs2 yükleniyorsa zaten rs1 kesinlikle yükelenecek
+                     rs2_veri_r_next = rs2_veri_i; // rs2 yukleniyorsa zaten rs1 kesinlikle yuklenecek
                      filtre_matris_dolu_next[filtre_matris_idx + 1'b1] = 1'b1;
                      filtre_matris_dolu_next[filtre_matris_idx] = 1'b1;
                      filtre_matris_idx_next = filtre_matris_idx + 2'b10;
@@ -192,6 +210,7 @@ module yapay_zeka_hizlandirici(
     end
     
     always @ (posedge clk_i) begin
+    
       if(filtre_sil_i || veri_sil_i) begin
          convolution_sonuc_r <= 32'd0;
          conv_idx <= 4'd0; 
@@ -215,7 +234,7 @@ module yapay_zeka_hizlandirici(
       end
    
       else begin
-        
+         cikis_ver <= cikis_ver_next;
          conv_yap_en_i_r <= conv_yap_en_i;
          conv_hazir_r <= conv_hazir_r_next;
          convolution_sonuc_r <= convolution_sonuc_r_next;
@@ -228,6 +247,10 @@ module yapay_zeka_hizlandirici(
          filtre_matris_dolu <= filtre_matris_dolu_next;
          
          conv_idx <= conv_idx_next;
+         
+         toplama_islemi <= toplama_islemi_next;
+         carpim_sonuc   <= carpim_sonuc_next;
+         
     
          case(mod_next)
            filtre_1 : begin
